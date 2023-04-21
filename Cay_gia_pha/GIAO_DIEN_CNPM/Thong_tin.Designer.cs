a39b64dc45d77dbcc6ace6b3381a9b24994932b6
PL Thuy Duong <thuyduongtd181003@gmail.com>
2023-04-21 18:20:16 +0700

    add SQL server

--- Cay_gia_pha/GIAO_DIEN_CNPM/Thong_tin.Designer.cs
+++ Cay_gia_pha/GIAO_DIEN_CNPM/Thong_tin.Designer.cs
@@ -41,7 +41,7 @@
             this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(255)))), ((int)(((byte)(185)))));
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(117, 146);
+            this.textBox1.Location = new System.Drawing.Point(114, 151);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(370, 45);
             this.textBox1.TabIndex = 2;
@@ -53,7 +53,7 @@
             this.guna2Separator1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(142)))), ((int)(((byte)(68)))));
             this.guna2Separator1.Location = new System.Drawing.Point(0, 187);
             this.guna2Separator1.Name = "guna2Separator1";
-            this.guna2Separator1.Size = new System.Drawing.Size(582, 19);
+            this.guna2Separator1.Size = new System.Drawing.Size(629, 19);
             this.guna2Separator1.TabIndex = 3;
             // 
             // richTextBox1
@@ -63,7 +63,7 @@
             this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richTextBox1.Location = new System.Drawing.Point(12, 212);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(557, 287);
+            this.richTextBox1.Size = new System.Drawing.Size(555, 313);
             this.richTextBox1.TabIndex = 5;
             this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
             // 
@@ -73,7 +73,7 @@
             this.guna2PictureBox1.Image = global::GIAO_DIEN_CNPM.Properties.Resources.Family;
             this.guna2PictureBox1.ImageRotate = 0F;
             this.guna2PictureBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.guna2PictureBox1.Location = new System.Drawing.Point(171, -2);
+            this.guna2PictureBox1.Location = new System.Drawing.Point(171, 7);
             this.guna2PictureBox1.Name = "guna2PictureBox1";
             this.guna2PictureBox1.Size = new System.Drawing.Size(197, 182);
             this.guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -85,7 +85,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(255)))), ((int)(((byte)(185)))));
-            this.ClientSize = new System.Drawing.Size(584, 552);
+            this.ClientSize = new System.Drawing.Size(569, 543);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.guna2PictureBox1);
--- Cay_gia_pha/GIAO_DIEN_CNPM/Trang_chinh.cs
+++ Cay_gia_pha/GIAO_DIEN_CNPM/Trang_chinh.cs
@@ -26,5 +26,32 @@ namespace GIAO_DIEN_CNPM
         {
 
         }
+
+        private void pcAboutus_Click(object sender, EventArgs e)
+        {
+            Thong_tin tt = new Thong_tin();
+            tt.Show();
+        }
+
+        private void PanelMain_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pcTimkiem_Click(object sender, EventArgs e)
+        {
+            Ho_so_gia_pha hoso = new Ho_so_gia_pha();
+            hoso.TopLevel = false;
+            hoso.AutoScroll = true;
+            PanelMain.Controls.Add(hoso);
+            hoso.Show();
+        }
+
+        private void guna2PictureBox2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Trang_chu tc = new Trang_chu();
+            tc.Show();
+        }
     }
 }
